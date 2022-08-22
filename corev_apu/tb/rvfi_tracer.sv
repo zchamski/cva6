d2fe9d9d217b241eb0572ce3f83fc2c17623e8e9
Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>
2022-08-22 09:23:26 +0200

    [RVFI tracer]: Enable proper detection of termination upon write to tohost.
    * corev_apu/tb/rvfi_tracer.sv (i): New.
      (prev_write_into_tohost): Ditto.
      (value_in_tohost): Ditto.
      (store_w_d_insn): Ditto.
    
    Signed-off-by: Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>

--- corev_apu/tb/rvfi_tracer.sv
+++ corev_apu/tb/rvfi_tracer.sv
@@ -24,20 +24,53 @@ module rvfi_tracer #(
 
   logic[riscv::XLEN-1:0] TOHOST_ADDR;
   int f;
+  int i;
   int unsigned SIM_FINISH;
+  logic prev_write_into_tohost[NR_COMMIT_PORTS-1:0];
+  logic [riscv::XLEN-1:0] value_in_tohost[NR_COMMIT_PORTS-1:0];
+  logic store_w_d_insn[NR_COMMIT_PORTS-1:0];
 
   initial begin
     f = $fopen($sformatf("trace_rvfi_hart_%h.dasm", HART_ID), "w");
     if (!$value$plusargs("time_out=%d", SIM_FINISH)) SIM_FINISH = 2000000;
+    TOHOST_ADDR = dtm_get_tohost_addr();
+    if (TOHOST_ADDR == '0) begin
+      $display("*** No valid address of 'tohost' (tohost == 0x%h), termination possible only by timeout or Ctrl-C!\n", TOHOST_ADDR);
+      $fwrite(f, "*** No valid address of 'tohost' (tohost == 0x%h), termination possible only by timeout or Ctrl-C!\n", TOHOST_ADDR);
+    end
+
+    // No termination condition at init.
+    for (i = 0; i < NR_COMMIT_PORTS-1; i++)
+      value_in_tohost[i] = '0;
+      prev_write_into_tohost[i] = 1'b0;
+      store_w_d_insn[i] = 1'b0;
   end
 
   final $fclose(f);
 
   logic [31:0] cycles;
+
   // Generate the trace based on RVFI
   logic [63:0] pc64;
   always_ff @(posedge clk_i) begin
     for (int i = 0; i < NR_COMMIT_PORTS; i++) begin
+
+      // TERMINATION in 64 bits: upon SD to TOHOST with bit 0 of MEM_WDATA == 1'b1
+      // and the two MSBytes of MEM_WDATA equal to zero.
+      // TOHOST is assumed aligned on an XLEN-bit boundary.
+      store_w_d_insn[i] = 0;
+
+      // Detect a write of non-zero value into tohost (64b: full dword, 32b: lower word).
+      // Postpone assignment to end of cycle for situations where mem_addr and mem_wdata
+      // are asserted for one cycle only and deasserted when 'valid' gets asserted.  This
+      // occurs for compressed writes.
+      if (rvfi_i[i].mem_addr == TOHOST_ADDR &&
+          rvfi_i[i].mem_wmask != '0 &&
+          rvfi_i[i].mem_wdata != '0) begin
+        prev_write_into_tohost[i] <= 1'b1;
+        value_in_tohost[i] <= rvfi_i[i].mem_wdata;
+      end
+
       pc64 = {{riscv::XLEN-riscv::VLEN{rvfi_i[i].pc_rdata[riscv::VLEN-1]}}, rvfi_i[i].pc_rdata};
       // print the instruction information if the instruction is valid or a trap is taken
       if (rvfi_i[i].valid) begin
@@ -47,7 +80,7 @@ module rvfi_tracer #(
         // Destination register information
         $fwrite(f, "%h 0x%h (0x%h)",
           rvfi_i[i].mode, pc64, rvfi_i[i].insn);
-        // Decode instruction to know if destination register is FP register
+        // Decode instruction to know if destination register is FP register.
         if ( rvfi_i[i].insn[6:0] == 7'b1001111 ||
              rvfi_i[i].insn[6:0] == 7'b1001011 ||
              rvfi_i[i].insn[6:0] == 7'b1000111 ||
@@ -62,47 +95,26 @@ module rvfi_tracer #(
           $fwrite(f, " x%d 0x%h\n",
             rvfi_i[i].rd_addr, rvfi_i[i].rd_wdata);
         end else $fwrite(f, "\n");
-        // if (rvfi_i[i].insn == 32'h00000073) begin
-        //   $finish(1);
-        //   $finish(1);
-        // end
-        // TERMINATION in 64 bits: upon SD to TOHOST with bit 0 of MEM_WDATA == 1'b1
-        // and the two MSBytes of MEM_WDATA equal to zero.
-        // TOHOST is assumed aligned on a 64-bit boundary.
-        // Treat first uncompressed insns.
-        if (rvfi_i[i].insn[31:16] != '0 &&
-            rvfi_i[i].insn[6:0]        == 7'b0100011  &&
-            rvfi_i[i].insn[14:12]      == 3'b011      ) begin
-          TOHOST_ADDR = dtm_get_tohost_addr();
-          if (TOHOST_ADDR == '0) begin
-            $display("*** No valid address of 'tohost' (tohost == 0x%h), termination possible only by timeout or Ctrl-C!\n", TOHOST_ADDR);
-            $fwrite(f, "*** No valid address of 'tohost' (tohost == 0x%h), termination possible only by timeout or Ctrl-C!\n", TOHOST_ADDR);
-          end
 
-          $fwrite(f, "### Got SD, TOHOST_ADDR = 0x%h\n", TOHOST_ADDR);
-        end
+        // Store detection: Check for uncompressed SD/SW insns.
+        if ( rvfi_i[i].insn[6:0]   == 7'b0100011 &&  /* 'store' opcode */
+            (rvfi_i[i].insn[14:12] == 3'b011     ||  /* SD */
+             rvfi_i[i].insn[14:12] == 3'b010))       /* SW */
+            store_w_d_insn[i] = 1;
 
-        if (rvfi_i[i].insn[31:16] == '0) begin // compressed instruction
-          if (rvfi_i[i].insn[15:13] == 3'b111 &&
-              rvfi_i[i].insn[1:0] == 2'b00) begin
-            $fwrite(f, "### Got a *compressed* SD instruction, TOHOST_ADDR = 0x%h\n", TOHOST_ADDR);
-          end
-        end
+        // Store detection: CHeck for compressed SD/SW instruction.
+        if (rvfi_i[i].insn[1:0] == 2'b00                             /* compressed, quadrant 0 */  &&
+            ((riscv::XLEN == 64 && rvfi_i[i].insn[15:13] == 3'b111)  /* rv64 && func3 == C.SD */   ||
+             rvfi_i[i].insn[15:13] == 3'b110                         /* func3 == C.SW */           ))
+          store_w_d_insn[i] = 1;
 
-        $fwrite(f, "###    mem_addr  = 0x%h\n", rvfi_i[i].mem_addr);
-        $fwrite(f, "###    mem_wmask = 0x%h\n", rvfi_i[i].mem_wmask);
-        $fwrite(f, "###    mem_wdata = 0x%h\n", rvfi_i[i].mem_wdata);
-        $fwrite(f, "###    mem_rmask = 0x%h\n", rvfi_i[i].mem_rmask);
-        $fwrite(f, "###    mem_rdata = 0x%h\n", rvfi_i[i].mem_rdata);
-
-        if (rvfi_i[i].mem_addr       == TOHOST_ADDR) begin
-          $fwrite(f, "### Got SD to TOHOST_ADDR (0x%h)\n", TOHOST_ADDR);
-          if (rvfi_i[i].mem_wdata[63:48] == '0 &&
-              rvfi_i[i].mem_wdata[0]     == 0'b1) begin
-            dtm_set_exitcode(rvfi_i[i].mem_wdata);
-            $finish(1);
-            $finish(1);
-          end
+        // If there was a write of non-zero vaue into (the lower half of) tohost,
+        // terminate the simulation.
+        if (store_w_d_insn[i] && prev_write_into_tohost[i]) begin
+          $display(">>> TERMINATING with exit value 0x%h at PC 0x%h\n", value_in_tohost[i], pc64);
+          dtm_set_exitcode(value_in_tohost[i]);
+          $finish(1);
+          $finish(1);
         end
       end else if (rvfi_i[i].trap)
         $fwrite(f, "exception : 0x%h\n", pc64);
